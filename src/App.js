d47b9c2b89e106e4a334c590752aeac64f475ae6
Prince Augustine <princeaugustine95@gmail.com>
2020-12-21 00:07:17 -0500

    This is third commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
 
 
   state={
-    persons:[{id:1,name:'Prince'},{id:2,name:'Tomin'},{id:3,name:'Vishak'}]
+    fname:'',
+    persons:[{id:1,name:'Prince'},{id:2,name:'Tomin'},{id:3,name:'Vishak'}],
+    
   }
 
 
@@ -21,25 +23,79 @@ class App extends Component {
   
   }
 
+  submitHandler=(e)=>{
+      e.preventDefault();
 
+      const newItem={
+        id:this.state.persons.length+1,
+        name:this.state.fname,
+
+      }
+
+      let arr=[];
+
+      arr=[...this.state.persons];
+
+      arr.push(newItem);
+
+      this.setState({
+        persons:arr
+      })
+
+  }
+
+  deleteHandler=(id)=>{
+
+    console.log(id);
+    let arr=[...this.state.persons];
+
+    const newarr=arr.filter(p=>{
+      return p.id!==id;
+    })
+
+    this.setState({
+      persons:newarr
+    })
+
+  }
+  fnameHandler=(e)=>{
+      this.setState({
+        fname:e.target.value
+      })
+  }
   render() {
 
+    let status=false;
     let message;
-
+      if(this.state.persons.length===1)
+      {
+        status=true;
+      }
     message=this.state.persons.map((person)=>{
 
-            return <Cardpanel id={person.id} name={person.name}/>
+            return <Cardpanel changecolor={status} key={person.id} id={person.id} name={person.name} delete={this.deleteHandler}/>
 
     })
 
     return (
       <div className="App">
         <h1>React List</h1>
-          <div class="row">
-              <div class="column">
+          <div className="row">
+              <div className="column">
                     <div>
-                      {message}
+                      <form onSubmit={this.submitHandler}>
+                      <div className="input-field col s6" >
+                          <input type="text" id="first_name" onChange={this.fnameHandler}/>
+                          <label htmlFor="first_name">First Name</label>
+                          <button className="btn-small teal darken-5">Submit</button>
+                      </div>
+                      </form>
+                  
+                      
                     </div>
+              </div>
+              <div className="column">
+                      {message}
               </div>
           </div>
       </div>
--- src/Cardpanel/Cardpanel.js
+++ src/Cardpanel/Cardpanel.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import './Cardpanel.css';
 
 const Cardpanel=(props)=>{
-
+            let id=props.id;
+            let nclass=props.changecolor?'headcolor':'';
+            
     return(
-        <div key={props.id} className="card-panel grey darken-3">
-                    <h1 className="center-align white-text">{props.name}</h1>
-                    <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+        <div  className="card-panel grey darken-3">
+                    <h1 className={`${nclass} center-align white-text`}>{props.name}</h1>
+                    <span className="white-text">I am a very simple card. I am good at containing small bits of information.
         I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
         </span>
-            <button class="btn-floating pulse right">Know More</button>
+            <button className="btn-floating pulse right" onClick={()=>props.delete(id)}><i className="material-icons">delete</i></button>
         </div>
     )
 
